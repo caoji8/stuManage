5e62543bb0f50a63a0776361ee25b546a4148294
caoji8 <www.972237177.com@qq.com>
2019-04-24 16:58:51 +0800

    新增了保存数据和读写数据

--- src/final/practice/Date.py
+++ src/final/practice/Date.py
@@ -1,5 +1,10 @@
 import json
+#写数据
 def saveDate(date,filename = "user.json"):
-    with open(filename,mode="w",encoding="utf-8") as f:
-        json.dump(date,f)
-saveDate("hello")
+    with open(filename,mode="w",encoding="utf-8") as sa:
+        json.dump(date,sa)
+#读数据
+def loadDate(filename="user.json"):
+    with open(filename,mode='r',encoding='utf-8') as op:
+        user_load = json.load(op)
+        return user_load
